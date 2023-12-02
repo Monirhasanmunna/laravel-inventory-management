59232e8c86967812a430cdd1a388cab379154809
MH Munna <monirhasanmunna1@gmail.com>
2023-12-03 02:21:45 +0600

    side bar changes

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -45,25 +45,16 @@
       <div class="main_container">
         <div class="col-md-3 left_col">
           <div class="left_col scroll-view">
-            <div class="navbar nav_title" style="border: 0;">
-              <a href="index.html" class="site_title"><i class="fa fa-paw"></i> <span>Gentelella Alela!</span></a>
+            <div class="navbar nav_title" style="border:1px  solid #D9DEE4;">
+              <a href="index.html" class="site_title"><i class="fa fa-paw"></i> <span>Laravel IMS</span></a>
             </div>
 
             <div class="clearfix"></div>
-
-            <!-- menu profile quick info -->
-            @include('layouts.backend.sidebar-menu-profile')
-            <!-- /menu profile quick info -->
-
-            <br />
 
             <!-- sidebar menu -->
             @include('layouts.backend.sidebar-menu')
             <!-- /sidebar menu -->
 
-            <!-- /menu footer buttons -->
-            @include('layouts.backend.sidebar-footer')
-            <!-- /menu footer buttons -->
           </div>
         </div>
 
--- resources/views/layouts/backend/sidebar-menu-profile.blade.php
+++ resources/views/layouts/backend/sidebar-menu-profile.blade.php
@@ -1,4 +1,4 @@
-<div class="profile clearfix">
+{{-- <div class="profile clearfix">
               <div class="profile_pic">
                 <img style="width:60px;height:60px;border-radius:50%;" src="{{Auth::user()->avatar != 'user.png' ? asset(Auth::user()->avatar)  : asset('backend/image/user.png')}}" alt="..." class="img-circle profile_img">
               </div>
@@ -6,4 +6,4 @@
                 <span>Welcome,</span>
                 <h2>John Doe</h2>
               </div>
-            </div>
+            </div> --}}
